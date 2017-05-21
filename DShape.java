70827670b34418bdb87792507550b88013032a26
Jake <jake83@gmail.com>
2017-05-20 17:49:44 -0700

    add knobs to selected shape

--- DShape.java
+++ DShape.java
@@ -1,6 +1,7 @@
 package finalproject_whiteboard;
 
 import java.awt.*;
+import java.util.*;
 
 public abstract class DShape implements ModelListener {
     
@@ -9,6 +10,8 @@ public abstract class DShape implements ModelListener {
     protected int dx = 0;
     protected int dy = 0;
     protected boolean isDragging = false;
+
+    protected final int KNOB_SIZE = 9;
 
     public DShape(DShapeModel model, Canvas canvas) {
         this.model = model;
@@ -43,6 +46,28 @@ public abstract class DShape implements ModelListener {
         model.setBounds((x - dx), (y - dy), bounds.width, bounds.height);
     }
 
+    public ArrayList<Point> getKnobs() {
+        Rectangle bounds = getBounds();
+        ArrayList<Point> knobs = new ArrayList<Point>();
+
+        int x = (int) bounds.getX();
+        int y = (int) bounds.getY();
+        int width = (int) bounds.getWidth();
+        int height = (int) bounds.getHeight();
+        
+        Point leftTop = new Point(x, y);
+        Point rightTop = new Point(x + width, y);
+        Point leftBottom = new Point(x, y + height);
+        Point rightBottom = new Point(x + width, y + height);
+
+        knobs.add(leftTop);
+        knobs.add(rightTop);
+        knobs.add(leftBottom);
+        knobs.add(rightBottom);
+
+        return knobs;
+    }
+
     public boolean containsPoint(Point point) {
         Rectangle bounds = getBounds();
         
@@ -60,8 +85,17 @@ public abstract class DShape implements ModelListener {
     public void draw(Graphics g, Boolean isSelected) {
         if (isSelected) {
             Rectangle bounds = model.getBounds();
-            g.setColor(Color.red);
-            g.drawString("X", bounds.x, bounds.y);
+            g.setColor(Color.black);
+            // g.drawString("X", bounds.x, bounds.y);
+            ArrayList<Point> knobs = getKnobs();
+
+            for (Point p : knobs) {
+                int x = (int) p.getX() - (KNOB_SIZE - 1) / 2;
+                int y = (int) p.getY() - (KNOB_SIZE - 1) / 2;
+                g.fillRect(x, y, KNOB_SIZE, KNOB_SIZE);
+
+                // System.out.println(p);
+            }
         }
     }
 }
